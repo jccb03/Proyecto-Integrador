88432475d7af8a9913332fb75829f0281d32f692
JeanCollado <jccb0001@ce.pucmm.edu.do>
2024-06-02 21:34:48 -0400

    registro usuario realizado

--- Web/Web/grails-app/controllers/turismo/UrlMappings.groovy
+++ Web/Web/grails-app/controllers/turismo/UrlMappings.groovy
@@ -14,7 +14,7 @@ class UrlMappings {
         get "/login"(controller: "login", action: "index")
         post "/login/iniciar_sesion"(controller: "login", action: "iniciar_sesion")
         get "/registrar_usuario"(controller: "general", action:"mostrarRegistroUsuario")
-        post "/salvar_usuario"(controller:"general", action:"salvar_usuario")
+        post "/registrar_usuario/salvar_usuario"(controller:"general", action:"salvar_usuario")
         "500"(view: '/error')
         "404"(view: '/notFound')
     }
